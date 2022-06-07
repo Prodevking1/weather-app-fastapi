d71b9f215b07a053c8f933638bc4cdae80896131
Murray Hat <77835100+Prodevking1@users.noreply.github.com>
2022-06-07 10:05:35 +0000

    final version

--- api/meteo.py
+++ api/meteo.py
@@ -3,6 +3,7 @@ from anyio import Any
 import fastapi
 from fastapi import Depends
 from models.location import Location
+from models.validation_errors import ValidationError
 from services import openweather_service
 
 
@@ -12,5 +13,9 @@ router = fastapi.APIRouter()
 async def meteo(loc: Location = Depends(),
           units: Optional[str] = 'metric'
           ):
-    report =  await openweather_service.get_reports(loc.city, loc.state, loc.country, units)
-    return report
+    try:
+        report =  await openweather_service.get_reports(loc.city, loc.state, loc.country, units)
+        return report
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
+    
--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,11 +1,30 @@
+from functools import cache
 from typing import Optional
-import requests
+from fastapi import Response
+import httpx
+from api.infrastructures.cache import set_weather
+
+from models.validation_errors import ValidationError
 
 
 async def get_reports (city:str, state:Optional[str], country:str, units:str):
     q = f"{city},{country}"
     key = '91f20fef6924822ab6748495265321b5'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={key}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={key}&units={units}'
     print(url)
-    response = requests.get(url)
-    return response.json()
+    async with httpx.AsyncClient() as client:
+        resp : Response = await client.get(url, timeout=60)
+        if resp.status_code != 200:
+            raise ValidationError(resp.text, resp.status_code)
+        
+    data = resp.json()
+    weather = data['weather']
+    main = data['main']
+    wind = data['wind']
+    sys = data['sys']
+    forecast = {'weather': weather,
+                'main': main,
+                'wind': wind,
+                'sys': sys}
+    
+    return forecast
